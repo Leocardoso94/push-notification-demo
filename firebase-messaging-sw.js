0709408549feb4e1242daa4aeaf25dcc8ce120c1
Leonardo Cardoso <leocardosoti@gmail.com>
2018-06-01 15:29:03 -0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: 'seu sender id'
+    messagingSenderId: '777608841115' // troque pelo seu sender id 
 });
 
 const messaging = firebase.messaging();
